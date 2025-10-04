f521dabe975e6efc28a8c92b8c8364cdec1c19c4
Maliikx <eljuoker@gmail.com>
2025-10-04 11:51:52 +0300

    updated selected work

--- app/data/selectedWork.js
+++ app/data/selectedWork.js
@@ -1,54 +1,52 @@
 export const selectedWork = [
-    {
-        'title' : 'Bedir Home Recliner',
-        'imgPath' : '/gifs/bedir.gif',
-        'description' : 'Complete redesign of the whole E-Commerce store and built an animated SEO-friendly landing page compatible across all screen sizes ',
-        'link' : 'https://bedireg.com',
-        'date' : '2025',
-        'job': 'Developed',
-        'hue': '007996'
+  {
+    title: "Bedir Home Recliner",
+    imgPath: "/gifs/bedir.gif",
+    description:
+      "Complete redesign of the whole E-Commerce store and built an animated SEO-friendly landing page compatible across all screen sizes ",
+    link: "https://bedireg.com",
+    date: "2025",
+    job: "Developed - Designed - SEO",
+    hue: "007996",
+  },
 
-    },
+  {
+    title: "Zanzibar Heritage Villas",
+    imgPath: "/gifs/zanzibar.gif",
+    description:
+      "Designed a custom animated hero section and transformed PDF slides into responsive web sections, maintaining brand aesthetics and cross-device performance",
+    link: "https://Heritage.Villas",
+    date: "2025",
+    job: "Developed",
+    hue: "e0c060",
+  },
 
-    {
-        'title' : 'Zanzibar Heritage Villas',
-        'imgPath' : '/gifs/zanzibar.gif',
-        'description' : 'Designed a custom animated hero section and transformed PDF slides into responsive web sections, maintaining brand aesthetics and cross-device performance',
-        'link' : 'https://Heritage.Villas',
-        'date' : '2025',
-        'job': 'Developed',
-        'hue': 'e0c060'
-
-    },
-    
-    {
-        'title' : 'Inci-flex-Egypt',
-        'imgPath' : '/inciFlexHero.png',
-        'description' : 'Developed a responsive, custom website for the Egyptian branch of INCI-FLEX, a multinational flexographic printing company.',
-        'link' : 'https://www.inciflexegypt.com/',
-        'date' : '2024',
-        'job': 'Developed - Designed - SEO',
-        'hue': ''
-
-    },
-    // {
-    //     'title' : 'Malak photograpy',
-    //     'imgPath' : './inciFlexHero.png',
-    //     'description' : 'Custom built website for the Egyptian branch of the international company INCI-FLEX.',
-    //     'link' : 'https://malak-photography.vercel.app/',
-    //     'date' : '2024',
-    //     'job': 'Developed - Designed - SEO'
-    // },
-    {
-        'title' : 'Soul Support',
-        'imgPath' : '/soulSupport/ss0.jpg',
-        'description' : 'My Graduation project an AI-powered Mental Health Support cross-platform App  which was Awarded #1 in the department',
-        'link' : 'https://github.com/Maliikx/soul_support',
-        'date' : '2025',
-        'job': 'Developed - Designed',
-        'hue': '01709A'
-    },
-    
-
-
-]
+  {
+    title: "Inci-flex-Egypt",
+    imgPath: "/inciFlexHero.png",
+    description:
+      "Developed a responsive, custom website for the Egyptian branch of INCI-FLEX, a multinational flexographic printing company.",
+    link: "https://www.inciflexegypt.com/",
+    date: "2024",
+    job: "Developed - Designed - SEO",
+    hue: "",
+  },
+  // {
+  //     'title' : 'Malak photograpy',
+  //     'imgPath' : './inciFlexHero.png',
+  //     'description' : 'Custom built website for the Egyptian branch of the international company INCI-FLEX.',
+  //     'link' : 'https://malak-photography.vercel.app/',
+  //     'date' : '2024',
+  //     'job': 'Developed - Designed - SEO'
+  // },
+  {
+    title: "Soul Support",
+    imgPath: "/soulSupport/ss0.jpg",
+    description:
+      "My Graduation project an AI-powered Mental Health Support cross-platform App  which was Awarded #1 in the department",
+    link: "https://github.com/Maliikx/soul_support",
+    date: "2025",
+    job: "Developed - Designed",
+    hue: "01709A",
+  },
+];
